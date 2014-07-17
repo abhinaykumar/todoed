b6d4210a07a9eec1dcb5182204a9307116f5c6ce
abhinay302 <abhinay302@gmail.com>
2014-07-18 01:27:11 +0530

    update color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,28 +13,6 @@ jQuery(document).ready(function() {
     ], {duration: 4000, fade: 750});
 
     /*
-        Countdown initializer
-    */
-    var now = new Date();
-    var countTo = 25 * 24 * 60 * 60 * 1000 + now.valueOf();
-    $('.timer').countdown(countTo, function(event) {
-        var $this = $(this);
-        switch(event.type) {
-            case "seconds":
-            case "minutes":
-            case "hours":
-            case "days":
-            case "weeks":
-            case "daysLeft":
-                $this.find('span.'+event.type).html(event.value);
-                break;
-            case "finished":
-                $this.hide();
-                break;
-        }
-    });
-
-    /*
         Tooltips
     */
     $('.social a.facebook').tooltip();
